d2879858f952d008f237389841f1426e9e1bdbff
jadams-cisco <gatoVoIP82!>
2017-07-10 16:55:50 -0400

    chained together 2 flask app.route statements to catch all URLs

--- flask-redirector/app.py
+++ flask-redirector/app.py
@@ -4,8 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'PUT', 'POST', 'PATCH', 'DELETE'])
-def index():
+@app.route('/', defaults={'path': ''}, methods=['GET', 'PUT', 'POST', 'PATCH', 'DELETE'])
+@app.route('/<path:path>', methods=['GET', 'PUT', 'POST', 'PATCH', 'DELETE'])
+def index(path):
     return app.send_static_file('index.html')
 
 @app.route('/background')
